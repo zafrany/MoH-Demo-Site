a4ffd890f51837a1d859023f581155fff08c925c
Yair Zafrany <karai.wot@gmail.com>
2022-03-24 00:06:16 +0200

    top almost done

--- public/index.js
+++ public/index.js
@@ -1,8 +1,23 @@
 let themeButton = document.querySelector(".theme-button");
+let background = document.querySelector(".content-container");
+let flexColor = document.querySelector(".top-cards-flex");
+let boxColor = document.querySelectorAll(".top-card-container");
+let exlamationMark = document.querySelectorAll(".card-h3-container .circle");
 
 themeButton.addEventListener('click', ()=>{
     themeButton.children[0].classList.toggle("displayNone");
     themeButton.children[1].classList.toggle("displayNone");
     themeButton.classList.toggle("theme-button__dark-color");
     themeButton.classList.toggle("theme-button__light-color");
+    background.classList.toggle("light-color-theme");
+    background.classList.toggle("dark-color-theme");
+    flexColor.classList.toggle("dark-color-theme");
+    flexColor.classList.toggle("light-color-theme");
+
+    for(let i = 0; i < exlamationMark.length; i++){
+        boxColor[i].classList.toggle("light-color-theme");
+        boxColor[i].classList.toggle("dark-color-theme__box-color");
+        exlamationMark[i].classList.toggle("light-circle-color");
+        exlamationMark[i].classList.toggle("dark-circle-color");
+    }
 })
