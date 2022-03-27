fdd6feb8e3a95abb7d1b497b00a8f02022dbbd92
zafrany <karai.wot@gmail.com>
2022-03-27 16:28:29 +0300

    re-factored

--- public/index.js
+++ public/index.js
@@ -1,28 +1,30 @@
 let themeButton = document.querySelector(".theme-button");
-let background = document.querySelector(".content-container");
-let flexColor = document.querySelector(".top-cards-flex");
-let boxColor = document.querySelectorAll(".top-card-container");
+let themeColors = document.querySelector(".content-container");
+let boxColor = document.querySelectorAll(".light-color-theme__box-color");
 let exlamationMark = document.querySelectorAll(".card-h3-container .circle");
-let secondRowflex = document.querySelector(".top-cards-flex-container");
 let circleArray = document.querySelectorAll(".light-circle-color");
+let tooltipArray = document.querySelectorAll(".tooltip");
 
 themeButton.addEventListener('click', ()=>{
     themeButton.children[0].classList.toggle("displayNone");
     themeButton.children[1].classList.toggle("displayNone");
     themeButton.classList.toggle("theme-button__dark-color");
     themeButton.classList.toggle("theme-button__light-color");
-    background.classList.toggle("light-color-theme");
-    background.classList.toggle("dark-color-theme");
-    flexColor.classList.toggle("dark-color-theme");
-    flexColor.classList.toggle("light-color-theme");
-    secondRowflex.classList.toggle("dark-color-theme__box-color");
-    secondRowflex.classList.toggle("light-color-theme__box-color");
+    themeColors.classList.toggle("background-light");
+    themeColors.classList.toggle("background-dark");
+    themeColors.classList.toggle("text-color-light");
+    themeColors.classList.toggle("text-color-dark");
+
+    for(let i = 0; i < boxColor.length; i++){
+        boxColor[i].classList.toggle("light-color-theme__box-color");
+        boxColor[i].classList.toggle("dark-color-theme__box-color");
+    }
 
     for(let i = 0; i < exlamationMark.length; i++){
-        boxColor[i].classList.toggle("light-color-theme");
-        boxColor[i].classList.toggle("dark-color-theme__box-color");
         exlamationMark[i].classList.toggle("light-circle-color");
         exlamationMark[i].classList.toggle("dark-circle-color");
+        tooltipArray[i].classList.toggle("dark-color-theme__box-color");
+        tooltipArray[i].classList.toggle("light-color-theme__box-color");
     }
 })
 
@@ -53,6 +55,16 @@ let topMockData2 = [
     ["27", "42.6%-"],
     ["399,532", "19%+", "19.1%"]
                       ];
+
+let settelmentsMockData = [
+    ["רעננה","89.7%","86.28%","70.69%","153.1","10"],
+    ["יבנה","74.76%","69.92%","52.18%","113.5","10"],
+    ["טירה","64.95%","56.31%","33.66%","19.3","6"],
+    ["אשדוד","70.95%","64.18%","47.31%","60.6","9.2"],
+    ["בני ברק","45.33%","33.38%","19.79%","7.5","5.1"],
+    ["חורה","25.12%","21.08%","8.92%","7.8","4.3"]
+];
+    
 
 let dataFields = document.querySelectorAll(".top-cards-flex .top-card-container");
 for(let i = 0; i < dataFields.length; i++) {
